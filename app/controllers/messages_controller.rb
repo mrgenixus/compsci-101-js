abc3fd83f75327b49829fe2c1a47c64a75d1fb74
Benjamin West <mrgenixus@gmail.com>
2015-02-19 12:18:26 -0700

    Fix the things

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,8 @@
-class MessagesController
+class MessagesController < ApplicationController
   def create
     Pusher.url = "http://012feed226050a90a1a9:bec69fdbb434aa35ba0e@api.pusherapp.com/apps/107687"
     Pusher['test_channel'].trigger('my_event', message_params)
+    render json: { status: "sent" }
   end
 
   private
