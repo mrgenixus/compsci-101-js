060dad6f82522373b4863caeee813e0b1c52f19b
Benjamin West <mrgenixus@gmail.com>
2015-02-19 11:52:07 -0700

    Added Pusher, Messages endpoint

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,13 @@
+class MessagesController
+  def create
+    Pusher.url = "http://012feed226050a90a1a9:bec69fdbb434aa35ba0e@api.pusherapp.com/apps/107687"
+    Pusher['test_channel'].trigger('my_event', message_params)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:author, :body, :image)
+  end
+end
+
