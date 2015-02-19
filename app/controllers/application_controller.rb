b1a1d184783eecd317b65aeebc8f04e358ec8fd6
Benjamin West <mrgenixus@gmail.com>
2015-02-19 11:58:10 -0700

    formatting ws, etc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
   before_filter :allow_cors
 
   def allow_cors
-     headers["Access-Control-Allow-Origin"] = "*"
-     headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
-     headers["Access-Control-Allow-Headers"] =
-       %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+    headers["Access-Control-Allow-Headers"] =
+      %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
 
-     render text: '' if request.request_method == "OPTIONS"
-   end
+    render text: '' if request.request_method == "OPTIONS"
+ end
 end
